4e95bf1560fdd2bfd96acde9fe8bab174de8369e
fernandocieri <fernandocieri@gmail.com>
2023-09-20 23:11:39 +0200

    Set basic layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,11 @@
-import './globals.css'
+'use client'
+
+import { useEffect } from 'react'
 import { Inter } from 'next/font/google'
+
+import './globals.css'
+import Header from '@/components/header/Header'
+import Menu from '@/components/menu/Menu'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,9 +15,37 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const container = document.getElementById('main-layout')
+
+    if (container) {
+      const handleScroll = (e) => {
+        e.preventDefault()
+        container.scrollLeft += e.deltaY * 2
+      }
+
+      container.addEventListener('wheel', handleScroll)
+
+      return () => {
+        container.removeEventListener('wheel', handleScroll)
+      }
+    }
+  }, [])
+
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div id='main-layout'>
+          <Header />
+          <div id='content-container' className='flex-row'>
+            <Menu />
+            <main>
+              {children}
+            </main>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
+
