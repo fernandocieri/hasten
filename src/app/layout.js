edb4ea590afd9331e1ed5146db15ed152aa5e366
fernandocieri <fernandocieri@gmail.com>
2023-09-20 23:27:45 +0200

    Move scroll logic into ScrollWrapper

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,5 @@
-'use client'
-
-import { useEffect } from 'react'
-import { Inter } from 'next/font/google'
-
 import './globals.scss'
-import Header from '@/components/header/Header'
-import Menu from '@/components/menu/Menu'
-
-const inter = Inter({ subsets: ['latin'] })
+import ScrollWrapper from '@/components/ScrollWrapper'
 
 export const metadata = {
   title: 'Hasten | Faded Telecast',
@@ -15,35 +7,12 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    const container = document.getElementById('main-layout')
-
-    if (container) {
-      const handleScroll = (e) => {
-        e.preventDefault()
-        container.scrollLeft += e.deltaY * 2
-      }
-
-      container.addEventListener('wheel', handleScroll)
-
-      return () => {
-        container.removeEventListener('wheel', handleScroll)
-      }
-    }
-  }, [])
-
   return (
     <html lang="es">
       <body>
-        <div id='main-layout'>
-          <Header />
-          <div id='content-container' className='flex-row'>
-            <Menu />
-            <main>
-              {children}
-            </main>
-          </div>
-        </div>
+        <ScrollWrapper>
+          {children}
+        </ScrollWrapper>
       </body>
     </html>
   )
---
+++ src/components/ScrollWrapper.js
@@ -0,0 +1,37 @@
+'use client'
+
+import { useEffect } from 'react'
+import Header from '@/components/header/Header'
+import Menu from '@/components/menu/Menu'
+
+export default function ScrollWrapper({ children }) {
+
+    useEffect(() => {
+        const container = document.getElementById('main-layout')
+
+        if (container) {
+            const handleScroll = (e) => {
+                e.preventDefault()
+                container.scrollLeft += e.deltaY * 1.5
+            }
+
+            container.addEventListener('wheel', handleScroll)
+
+            return () => {
+                container.removeEventListener('wheel', handleScroll)
+            }
+        }
+    }, [])
+
+    return (
+        <div id='main-layout'>
+            <Header />
+            <div id='content-container' className='flex-row'>
+                <Menu />
+                <main>
+                    {children}
+                </main>
+            </div>
+        </div>
+    )
+}
