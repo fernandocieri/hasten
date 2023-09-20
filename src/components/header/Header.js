ffa93f95811bb50eb28e759eb9d367900eff6176
fernandocieri <fernandocieri@gmail.com>
2023-09-20 23:12:13 +0200

    Create Header and Menu component

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import './header.scss';
+import Nav from '@components/nav/Nav';
+
+export default function Header() {
+  return (
+    <header className='flex-row'>
+      <Nav/>
+    </header>
+  )
+}
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,17 @@
+import './menu.scss';
+import Image from "next/image"
+import Nav from '@components/nav/Nav';
+
+export default function Menu() {
+  return (
+    <aside id="aside-menu" className="flex flex-col">
+      <div className="hasten-logo">
+        <Image src={'/png/hasten-logo.png'} fill priority alt="Logo de Hasten" />
+      </div>
+
+      <div className="aside-spacer"></div>
+
+      <Nav />
+    </aside>
+  )
+}
