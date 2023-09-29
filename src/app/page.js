e099d12448ffc9994e40223c0a657229ba79a98e
fernandocieri <fernandocieri@gmail.com>
2023-09-29 17:28:12 +0200

    Start working on landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
-import Image from 'next/image'
+import './styles/home.scss'
+import Image from "next/image"
 
 export default function Home() {
+
   return (
-    <main className="">
-      <h1>Hello World</h1>
-      
-    </main>
+    <div className='flex-row'>
+      <div id="first-fold">
+        <div id="hasten-logo-tipografico-section">
+          <Image src={'/svg/hasten-logo-tipografico.svg'} fill priority alt="Logo tipográfico de Hasten" />
+        </div>
+      </div>
+
+      <div id='second-fold' className='flex-col'>
+        <div id="persona-quitando-pintura-section">
+          <Image src={'/jpg/persona-quitando-pintura.jpg'} fill priority alt="Foto de una persona mirando extrañada  ala cámara mientras qiita a pintura de una pared con su mano" />
+        </div>
+
+        <div id="claqueta-section">
+          <Image src={'/jpg/claqueta.jpg'} fill priority alt="Foto de una claqueta de las grabaciones de Hasten" />
+        </div>
+      </div>
+    </div>
   )
 }
