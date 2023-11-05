6a3ed2c45b5a75486f10a7ac544141babcc30ef4
fernandocieri <fernandocieri@gmail.com>
2023-11-05 21:03:34 +0100

    Fix streched images and add correct hasten logo

--- src/app/page.js
+++ src/app/page.js
@@ -7,17 +7,17 @@ export default function Home() {
     <div className='flex-row'>
       <div id="first-fold">
         <div id="hasten-logo-tipografico-section">
-          <Image src={'/svg/hasten-logo-tipografico.svg'} fill priority alt="Logo tipográfico de Hasten" />
+          <Image src={'/png/hasten-logo-tipografico.png'} fill priority alt="Logo tipográfico de Hasten" />
         </div>
       </div>
 
       <div id='second-fold' className='flex-col'>
         <div id="persona-quitando-pintura-section">
-          <Image src={'/jpg/persona-quitando-pintura.jpg'} fill priority alt="Foto de una persona mirando extrañada  ala cámara mientras qiita a pintura de una pared con su mano" />
+          <Image src={'/png/persona-quitando-pintura_cropped.png'} fill priority alt="Foto de una persona mirando extrañada  ala cámara mientras qiita a pintura de una pared con su mano" />
         </div>
 
         <div id="claqueta-section">
-          <Image src={'/jpg/claqueta.jpg'} fill priority alt="Foto de una claqueta de las grabaciones de Hasten" />
+          <Image src={'/png/claqueta_cropped.png'} fill priority alt="Foto de una claqueta de las grabaciones de Hasten" />
         </div>
       </div>
     </div>
