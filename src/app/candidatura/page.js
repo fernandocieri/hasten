e0019c7125dbc4c5da518be4761957444b57e2cb
fernandocieri <fernandocieri@gmail.com>
2023-11-12 15:28:56 +0100

    Move CandidatureForm into a componetn and add metatags to page

--- src/app/candidatura/page.js
+++ src/app/candidatura/page.js
@@ -1,111 +1,23 @@
-'use client'
+import CandidatureForm from "./CandidatureForm";
 
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import "./candidature.scss";
+const metaTitle = "Participa en HASTEN | Envía tu Candidatura como Filmmaker";
+const metaDescription = "¿Eres filmmaker y quieres desafiar tus límites? Presenta tu candidatura para ser parte del próximo ciclo de HASTEN. Rellena el formulario, comparte tu visión y únete a nuestro laboratorio fílmico."
+export const metadata = {
+  title: metaTitle,
+  description: metaDescription,
+  openGraph: {
+    title: metaTitle,
+    description: metaDescription,
+  }
+}
 
 export default function Candidature() {
-    const [submitSuccess, setSubmitSuccess] = useState(false);
-    const [submitError, setSubmitError] = useState(false);
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
-    const isFormError = Object.keys(errors).length > 0;
 
-    let buttonClass = '';
-    if (submitSuccess) {
-        buttonClass = 'submit-success';
-    } else if (submitError || isFormError) {
-        buttonClass = 'submit-error';
-    }
-
-    let buttonText = '¡ENVIAR MI CANDIDATURA!';
-    if (submitSuccess) {
-        buttonText = 'FORMULARIO ENVIADO CON ÉXITO';
-    } else if (submitError) {
-        buttonText = 'ERROR AL ENVIAR EL FORMULARIO';
-    } else if (isFormError) {
-        buttonText = 'REVISA TU CANDIDATURA, ¡HAY ERRORES!';
-    }
-
-    const onSubmit = async data => {
-        setSubmitSuccess(false);
-        setSubmitError(false);
-
-        try {
-            // Simulación de llamada a la API, reemplazar con la llamada real
-            // const response = await myApiCall(data);
-            // if (!response.ok) throw new Error('API call failed');
-            console.log('Datos enviados:', data); // Aquí se manejaría la llamada a la API
-
-            setSubmitSuccess(true);
-            reset();
-        } catch (error) {
-            console.error('Error al enviar el formulario:', error);
-            setSubmitError(true);
-        }
-    };
-
-    return (
-        <section id="candidature">
-            <h2 className='main-title'>REINVENTA EL <span className='basteleur'>C</span>INE</h2>
-            <form id="candidature-form" onSubmit={handleSubmit(onSubmit)}>
-                <div className={`field-container flex-row ${errors.name && 'field-error'}`}>
-                    <label className='field-label'>
-                        NOMBRE:
-                    </label>
-                    <input
-                        className='field-input'
-                        type='text'
-                        {...register('name', { required: 'Este campo es obligatorio' })}
-                    />
-                </div>
-
-                <div className={`field-container flex-row ${errors.email && 'field-error'}`}>
-                    <label className='field-label'>
-                        CORREO:
-                    </label>
-                    <input
-                        className='field-input'
-                        type='text'
-                        {...register('email', {
-                            required: 'Este campo es obligatorio',
-                            pattern: {
-                                value: /^\S+@\S+$/i,
-                                message: 'Formato de correo no válido'
-                            }
-                        })}
-                    />
-                </div>
-
-                <div className={`field-container flex-row ${errors.preferredGenre && 'field-error'}`}>
-                    <label className='field-label'>
-                        GÉNERO DE CINE PREFERIDO:
-                    </label>
-                    <input
-                        className='field-input'
-                        type='text'
-                        {...register('preferredGenre', { required: 'Este campo es obligatorio' })}
-                    />
-                </div>
-
-                <div className={`field-container flex-row textarea ${errors.motivationLetter && 'field-error'}`}>
-                    <label className='field-label textarea'>
-                        CARTA MOTIVACIÓN:
-                    </label>
-                    <textarea
-                        className='field-input textarea'
-                        {...register('motivationLetter', { required: 'Este campo es obligatorio' })}
-                    />
-                </div>
-
-                <button
-                    className={`candidature-submit ${buttonClass}`}
-                    type="submit"
-                    disabled={submitSuccess}
-                >
-                    {buttonText}
-                </button>
-            </form>
-        </section>
-    );
+  return (
+    <section id="candidature">
+      <h2 className='main-title'>REINVENTA EL <span className='basteleur'>C</span>INE</h2>
+      <CandidatureForm />
+    </section>
+  );
 }
